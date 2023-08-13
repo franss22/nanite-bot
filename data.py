d422615bab6874400d1595f1b6034781ab9b3fa3
franss22 <franortizz2001@gmail.com>
2023-08-13 17:18:30 -0400

    Stuff

--- data.py
+++ data.py
@@ -1,85 +1,138 @@
 import redis
+import os
 from redis.commands.json.path import Path
 import redis.commands.search.aggregation as aggregations
 import redis.commands.search.reducers as reducers
 from redis.commands.search.field import TextField, NumericField, TagField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import NumericFilter, Query
+import functools
+
 
 class DatabaseMissingValueException(Exception):
     pass
+
 
 def check(val, name=""):
     if val is None:
         raise DatabaseMissingValueException(f"Value {name+' '} is None in database")
     return val
 
+
 CLASSES = {
-    "BIOH":"Biohacker",
-    "ENVO":"Envoy",
-    "EVOL":"Evolucionist",
-    "MECH":"Mechanic",
-    "MYST":"Mystic",
-    "NANO":"Nanocyte",
-    "OPER":"Operative",
-    "PREC":"Precog",
-    "SOLA":"Solarian",
-    "SOLD":"Soldier",
-    "TECH":"Technomancer",
-    "VANG":"Vanguard",
-    "WITC":"Witchwarper",
+    "BIOH": "Biohacker",
+    "ENVO": "Envoy",
+    "EVOL": "Evolucionist",
+    "MECH": "Mechanic",
+    "MYST": "Mystic",
+    "NANO": "Nanocyte",
+    "OPER": "Operative",
+    "PREC": "Precog",
+    "SOLA": "Solarian",
+    "SOLD": "Soldier",
+    "TECH": "Technomancer",
+    "VANG": "Vanguard",
+    "WITC": "Witchwarper",
 }
 
-def new_pc(_name:str, _floor:int, _class:str):
+
+def new_pc(_name: str, _floor: int, _class: str):
     if _class not in CLASSES.keys():
         raise Exception("New PC: Invalid class key")
     return {
-        "name":_name,
-        "credits":0,
-        "floor":int(_floor),
-        "dt":0,
-        "classes":[_class]
+        "name": _name,
+        "credits": 0,
+        "floor": int(_floor),
+        "dt": 0,
+        "classes": [_class],
     }
 
+
 class database:
-    r:redis.Redis = None
-    
-    def __init__(self, connection:redis.Redis):
-        self.r = connection
+    r: redis.Redis = None
+
+    def __init__(self):
+        # self.connect()
+        pass
+
+    def connect(self):
+        self.r = redis.StrictRedis.from_url(
+            os.environ.get("REDIS_URL"), 
+            health_check_interval=30,
+            socket_connect_timeout=5,
+            retry_on_timeout=True,
+            socket_keepalive=True
+        )
+
+    def disconnect(self):
+        self.r.close()
+
+    def check_connection(self, retry=5):
+        pass
+        # if retry <= 0:
+        #     raise Exception("Connection to redis server lost, retries finished.")
+        # try:
+        #     self.r.ping()
+        # except:
+        #     self.connect()
+        #     self.check_connection(retry - 1)
+
+    def redis_connection(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            self.connect()
+            a = func(self, *args, **kwargs)
+            self.disconnect()
+            return a
+        return wrapper
+
+    @redis_connection
     def user_list(self):
         return self.r.lrange("USER_LIST", 0, -1)
+
+    @redis_connection
     def get_pc_name(self, user_id):
+        self.check_connection()
         return check(self.r.get(user_id)).decode("utf-8")
-    
+
+    @redis_connection
     def get_pc(self, user_id, pc_name):
         return check(self.r.json().get(f"{user_id}:{pc_name}", "$"))[0]
 
+    @redis_connection
     def register(self, user_id, pc_name, pc_floor, pc_class):
         pc = new_pc(pc_name, pc_floor, pc_class)
         self.r.json().set(f"{user_id}:{pc_name}", "$", pc)
         self.r.set(user_id, pc_name)
         self.r.lpush("USER_LIST", user_id)
 
+    @redis_connection
     def unregister(self, user_id):
         self.r.delete(user_id)
         self.r.lrem("USER_LIST", 1, user_id)
 
+    @redis_connection
     def update_pc(self, user_id, pc_name, updated_pc):
         self.r.json().set(f"{user_id}:{pc_name}", "$", updated_pc)
 
-    def replace_val(self, user_id, pc_name, key:str, new_val):
+    @redis_connection
+    def replace_val(self, user_id, pc_name, key: str, new_val):
         self.r.json().set(f"{user_id}:{pc_name}", f"$.{key}", new_val)
 
-    def update_int_val(self, user_id, pc_name, key:str, delta:int, replace=False, can_go_negative=False):
+    @redis_connection
+    def update_int_val(
+        self,
+        user_id,
+        pc_name,
+        key: str,
+        delta: int,
+        replace=False,
+        can_go_negative=False,
+    ):
         old_value = check(self.r.json().get(f"{user_id}:{pc_name}", f"$.{key}"))[0]
-        updated_value = delta if replace else old_value+delta
-        if not can_go_negative and updated_value<0:
+        updated_value = delta if replace else old_value + delta
+        if not can_go_negative and updated_value < 0:
             return (False, old_value, old_value)
         else:
             self.r.json().set(f"{user_id}:{pc_name}", f"$.{key}", updated_value)
             return (True, old_value, updated_value)
-
-    
-
-
-
--- main.py
+++ main.py
@@ -12,25 +12,27 @@ import os
 from data import database, CLASSES
 from data import DatabaseMissingValueException as DBNone
 
+
 class CharacterNotFoundException(Exception):
     pass
+
 
 intents = nextcord.Intents.default()
 intents.message_content = True
 
-conn= redis.StrictRedis.from_url(os.environ.get("REDIS_URL"))
-DEBUG=True
+DEBUG = False
 
-db = database(conn)
+db = database()
 
 bot = commands.Bot(command_prefix=")", intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f"We have logged in as {bot.user}")
 
 
-async def is_servicio_tecnico(ctx:Ctx):
+async def is_servicio_tecnico(ctx: Ctx):
     role = nextcord_get(ctx.guild.roles, name="Servicio tecnico")
     return role in ctx.author.roles
 
@@ -39,27 +41,32 @@ class Register(menus.ButtonMenu):
     def __init__(self, user_id, pc_name, floor):
         super().__init__(disable_buttons_after=True)
         for key, name in CLASSES.items():
-            async def CB(itrx:nextcord.Interaction, k=key, n=name):
+
+            async def CB(itrx: nextcord.Interaction, k=key, n=name):
                 db.register(user_id, pc_name, floor, k)
                 await itrx.send(f"Personaje registrado con clase {n}!")
                 self.stop()
+
             butt = Button(label=name)
             butt.callback = CB
             self.add_item(butt)
+
     async def send_initial_message(self, ctx, channel):
-        return await channel.send(f'Selecciona la clase de tu PJ.', view=self)
+        return await channel.send(f"Selecciona la clase de tu PJ.", view=self)
 
 
 @bot.command()
-async def register(ctx:Ctx, nombre:str, piso:int):
+async def register(ctx: Ctx, nombre: str, piso: int):
     user_id = ctx.author.id
     try:
         pc_name = db.get_pc_name(user_id)
-        await ctx.send(f"Ya tienes el PJ {pc_name} asignado. Si quieres registrar un nuevo PJ, elimina el anterior primero.")
+        await ctx.send(
+            f"Ya tienes el PJ {pc_name} asignado. Si quieres registrar un nuevo PJ, elimina el anterior primero."
+        )
         return
     except DBNone:
         await Register(user_id, nombre, piso).start(ctx)
-        
+
 
 def get_pc_data_from_user_id(user_id):
     try:
@@ -68,7 +75,8 @@ def get_pc_data_from_user_id(user_id):
     except DBNone as e:
         raise CharacterNotFoundException(str(e))
 
-def gets_character(func):
+
+def uses_database(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         if DEBUG:
@@ -77,96 +85,126 @@ def gets_character(func):
             try:
                 return await func(*args, **kwargs)
             except CharacterNotFoundException:
-                ctx:Ctx = args[0]
+                ctx: Ctx = args[0]
                 await ctx.send("Error: no tienes un PJ registrado")
             except DBNone as e:
-                ctx:Ctx = args[0]
+                ctx: Ctx = args[0]
                 await ctx.send(f"Error, {str(e)}")
+            except Exception as e:
+                ctx: Ctx = args[0]
+                await ctx.send(f"Unexpected error, {str(e)}")
+
     return wrapper
 
 
-
 @bot.command()
-@gets_character
-async def status(ctx:Ctx):
+@uses_database
+async def status(ctx: Ctx):
     pc_data = get_pc_data_from_user_id(ctx.author.id)
-    name= pc_data["name"]
-    floor= pc_data["floor"]
-    credits= pc_data["credits"]
-    dt= pc_data["dt"]
-    classes= pc_data["classes"]
-    class_names= [CLASSES[key] for key in classes]
-    class_string= ", ".join(class_names)
-    message=f"""## Status de {name}
+    name = pc_data["name"]
+    floor = pc_data["floor"]
+    credits = pc_data["credits"]
+    dt = pc_data["dt"]
+    classes = pc_data["classes"]
+    class_names = [CLASSES[key] for key in classes]
+    class_string = ", ".join(class_names)
+    message = f"""## Status de {name}
 - **Piso:** {floor}
 - **Credits:** {credits}
 - **Downtime:** {dt} dias
 - **Clase{"s" if len(classes)>1 else ""}:** {class_string}"""
     await ctx.send(message)
 
+
 @bot.command()
-@gets_character
-async def creds(ctx:Ctx, delta:int=0):
+@uses_database
+async def creds(ctx: Ctx, delta: int = 0):
     user_id = ctx.author.id
     pc_name = db.get_pc_name(user_id)
     success, old, updated = db.update_int_val(user_id, pc_name, "credits", delta)
-    if delta==0:
-        message=f"`Créditos de {pc_name}:` {updated}"
+    if delta == 0:
+        message = f"`Créditos de {pc_name}:` {updated}"
     elif success:
-        message=f"""`Créditos de {pc_name} actualizado:` {old} -> {updated} ({'+' if delta>0 else ''}{delta})"""
+        message = f"`Créditos de {pc_name} actualizado:` {old} -> {updated} ({'+' if delta>0 else ''}{delta})"
     else:
-        message=f"{pc_name} no tiene suficientes créditos para esta operación ({old})"
+        message = f"{pc_name} no tiene suficientes créditos para esta operación ({old})"
     await ctx.send(message)
 
+
 @bot.command()
-@gets_character
-async def dt(ctx:Ctx, delta:int=0):
+@uses_database
+async def dt(ctx: Ctx, delta: int = 0):
     user_id = ctx.author.id
     pc_name = db.get_pc_name(user_id)
     success, old, updated = db.update_int_val(user_id, pc_name, "dt", delta)
-    if delta==0:
-        message=f"`Dowtime de {pc_name}:` {updated}"
+    if delta == 0:
+        message = f"`Dowtime de {pc_name}:` {updated}"
     elif success:
-        message=f"""`Downtime de {pc_name} actualizado:` {old} -> {updated} ({'+' if delta>0 else ''}{delta})"""
+        message = f"`Downtime de {pc_name} actualizado:` {old} -> {updated} ({'+' if delta>0 else ''}{delta})"
     else:
-        message=f"{pc_name} no tiene suficiente donwtime para esta operación ({old})"
+        message = f"{pc_name} no tiene suficiente donwtime para esta operación ({old})"
     await ctx.send(message)
+
 
 @bot.command()
 @commands.check(is_servicio_tecnico)
-async def reset_dt(ctx:Ctx, dt:int):
+@uses_database
+async def reset_dt(ctx: Ctx, dt: int):
     users = db.user_list()
     i = 0
     for user in users:
         user_id = user.decode("utf-8")
         pc_name = db.get_pc_name(user_id)
         db.replace_val(user_id, pc_name, "dt", dt)
-        i+=1
+        i += 1
 
     await ctx.send(f"Cambiado el dt de {i} PJs.")
 
+
 @bot.command()
 @commands.check(is_servicio_tecnico)
-async def backup(ctx:Ctx):
+@uses_database
+async def reset_dt(ctx: Ctx, dt: int):
+    users = db.user_list()
+    i = 0
+    for user in users:
+        user_id = user.decode("utf-8")
+        pc_name = db.get_pc_name(user_id)
+        db.update_int_val(user_id, pc_name, "dt", dt)
+        i += 1
+
+    await ctx.send(f"Sumado {dt} días al dt de {i} PJs.")
+
+@bot.command()
+@commands.check(is_servicio_tecnico)
+@uses_database
+async def backup(ctx: Ctx):
     users = db.user_list()
     txt = ""
     for user in users:
         data = get_pc_data_from_user_id(user.decode("utf-8"))
-        txt+=user.decode("utf-8")+"\t"+json.dumps(data)+"\n"
+        txt += user.decode("utf-8") + "\t" + json.dumps(data) + "\n"
 
-    await ctx.send("Backup completado", file=nextcord.File(fp=io.BytesIO(txt.encode("utf-8")), filename="backup.txt"))
+    await ctx.send(
+        "Backup completado",
+        file=nextcord.File(fp=io.BytesIO(txt.encode("utf-8")), filename="backup.txt"),
+    )
+
 
 @bot.command()
-@gets_character
-async def piso(ctx:Ctx, new_floor:int):
+@uses_database
+async def piso(ctx: Ctx, new_floor: int):
     user_id = ctx.author.id
     pc_name = db.get_pc_name(user_id)
-    success, old, updated = db.update_int_val(user_id, pc_name, "credits", new_floor, replace=True, can_go_negative=True)
+    success, old, updated = db.update_int_val(
+        user_id, pc_name, "credits", new_floor, replace=True, can_go_negative=True
+    )
     if success:
-        message=f"""`{pc_name} se mueve de piso:` {old} -> {updated}"""
+        message = f"""`{pc_name} se mueve de piso:` {old} -> {updated}"""
     else:
-        message=f"Hubo un error cambiando a {pc_name} de piso. Esto no debiera pasar, llame a Pancho pls."
+        message = f"Hubo un error cambiando a {pc_name} de piso. Esto no debiera pasar, llame a Pancho pls."
     await ctx.send(message)
+
 
 class UpdateClasses(menus.ButtonMenu):
     def __init__(self, user_id, pc_name, add=True):
@@ -174,7 +212,7 @@ class UpdateClasses(menus.ButtonMenu):
         super().__init__(disable_buttons_after=True)
         pc_data = db.get_pc(user_id, pc_name)
         classes = pc_data["classes"]
-        self.not_enough_classes=(classes==1 and not add)
+        self.not_enough_classes = classes == 1 and not add
 
         if self.not_enough_classes:
             return
@@ -187,45 +225,60 @@ class UpdateClasses(menus.ButtonMenu):
             class_name = CLASSES[class_key]
             cc = classes.copy()
             cc.append(class_key) if add else cc.remove(class_key)
-            async def CB(itrx:nextcord.Interaction, n=class_name, c=cc):
+
+            async def CB(itrx: nextcord.Interaction, n=class_name, c=cc):
                 db.replace_val(user_id, pc_name, "classes", c)
                 await itrx.send(f'Clase {n} {"añadida" if self.add else "quitada"}')
                 self.stop()
+
             butt = Button(label=class_name)
             butt.callback = CB
             self.add_item(butt)
+
     async def send_initial_message(self, ctx, channel):
         if self.not_enough_classes:
-            return await channel.send(f'No puedes eliminar tu única clase. Añade otra clase primero.', view=self)
-        return await channel.send(f'Selecciona la clase que quieres {"añadir" if self.add else "quitar"}.', view=self)
-    
+            return await channel.send(
+                f"No puedes eliminar tu única clase. Añade otra clase primero.",
+                view=self,
+            )
+        return await channel.send(
+            f'Selecciona la clase que quieres {"añadir" if self.add else "quitar"}.',
+            view=self,
+        )
+
+
 @bot.command()
-@gets_character
-async def añadir_clase(ctx:Ctx):
+@uses_database
+async def añadir_clase(ctx: Ctx):
     user_id = ctx.author.id
     pc_name = db.get_pc_name(user_id)
     await UpdateClasses(user_id, pc_name, add=True).start(ctx)
-    
+
+
 @bot.command()
-@gets_character
-async def quitar_clase(ctx:Ctx):
+@uses_database
+async def quitar_clase(ctx: Ctx):
     user_id = ctx.author.id
     pc_name = db.get_pc_name(user_id)
     await UpdateClasses(user_id, pc_name, add=False).start(ctx)
 
+
 @bot.command()
-async def unregister(ctx:Ctx, secure:str=""):
-    if secure=="seguro_que_quiero_borrar_mi_pj":
+@uses_database
+async def unregister(ctx: Ctx, secure: str = ""):
+    if secure == "seguro_que_quiero_borrar_mi_pj":
         db.unregister(ctx.author.id)
         await ctx.send("Tu PJ ha sido desregistrado!")
     else:
-        await ctx.send("Manda este comando con el parametro 'seguro_que_quiero_borrar_mi_pj' para desregistrar a tu PJ.")
-
+        await ctx.send(
+            "Manda este comando con el parametro 'seguro_que_quiero_borrar_mi_pj' para desregistrar a tu PJ."
+        )
 
 
 @bot.command()
-async def hello(ctx:Ctx):
+async def hello(ctx: Ctx):
     await ctx.send("Hello!")
+
 
 # with open("secrets", "r") as f:
 #     token = f.read()
